47447aae6598ac2a0c14a9bd4daf1909639a1eec
Juan Carlos <Juancarlosrb31@gmail.com>
2025-04-22 11:53:04 +0200

    primeros ejercicios

---
+++ Ejercicio1.py
@@ -0,0 +1,22 @@
+## Declarar variables#
+#nombre = input("Cual es tu nombre?")
+edad = int(input("cual es tu edad? "))
+# print(f"hola {nombre}! tienes {edad} años") 
+
+#def saludar (nombre):
+ #   print(f"¡Hola,{nombre}! Bienvenido@ al taller") # imprime la funcion saludar 
+#saludar (nombre)
+
+#for recorrer  in range (1,10):
+ #   print(recorrer)
+    
+
+def esMayorDeEdad(edad):
+    if edad >= 18:
+        return "Eres Mayor de Edad"
+    else:
+        return "Eres menor de Edad"
+print(esMayorDeEdad(edad))
+            
+            
+            
---
+++ hola.py
@@ -0,0 +1,5 @@
+nombre = "Juan"
+edad = 40 
+float = 0.12 
+bool = True or False
+
