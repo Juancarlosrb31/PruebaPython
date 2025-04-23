b61b66e789394700f7cefef1018c0d44674616da
Juan Carlos <Juancarlosrb31@gmail.com>
2025-04-23 13:56:43 +0200

    actualizacion

---
+++ CalcularElAñoDeNacimiento.py
@@ -0,0 +1,13 @@
+#Crea un script que te pregunta:
+#Nombre
+#Año de nacimiento
+#Ahora con un cálculo
+#Calcula la edad a partir del año de nacimiento :)
+
+
+nombre = input("Cual es tu nombre?")
+año_nacimiento = int(input("En que año naciste? " ))
+año_actual = 2025
+edad = año_actual - año_nacimiento
+
+print(edad, "Te estas poniendo viejo") 
